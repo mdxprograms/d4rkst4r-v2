700871e48e8077eb93fe781e7df9ed94b84675b1
Josh Waller <mdx.programs@gmail.com>
2019-12-12 17:04:36 -0500

    Adding working version with overlay explanation

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,9 +1,43 @@
 (ns app.core
   (:require [reagent.core :as r]
-            [app.hello :refer [hello]]))
+            [ajax.core :refer [GET]]
+            [app.info-card :refer [info-card]])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
+
+(def explanation (r/atom nil))
+(def show-explanation (r/atom false))
+(def results (r/atom nil))
+
+(defn handle-item-click [text]
+  (reset! show-explanation true)
+  (reset! explanation text))
+
+(defn handle-overlay-click []
+  (reset! show-explanation false)
+  (reset! explanation ""))
+
+(defn overlay []
+  [:div#overlay
+   {:on-click #(handle-overlay-click)}
+   [:div#text @explanation]])
+
+(defn cards []
+  (let [get-results (fn [] (GET "apods.json" :handler (fn [r] (reset! results r))))]
+    (get-results)
+    (fn []
+      [:div.article
+       [:div.cf.pa2.flex.flex-wrap
+        (for [item @results]
+          ^{:key (get-in item ["title"])}
+          [info-card item handle-item-click])]])))
+
+(defn app []
+  [:div.app
+   (when @show-explanation [overlay])
+   [cards]])
 
 (defn ^:dev/after-load start []
-  (r/render [hello] (.getElementById js/document "app")))
+  (r/render [app] (.getElementById js/document "app")))
 
 (defn ^:export main []
   (start))
---
+++ src/app/info_card.cljs
@@ -0,0 +1,16 @@
+(ns app.info-card
+  (:require [reagent.core :as r]))
+
+(defn is-image [source]
+  (or (clojure.string/ends-with? source "jpg")
+      (clojure.string/ends-with? source "png")))
+
+(defn info-card [item handle-item-click]
+  [:div.w-100.w-50-m.w-25-l.pa3.card
+   {:on-click (fn [] (handle-item-click (get-in item ["explanation"])))}
+   [:img.db.w-100.br2.br--top.shadow-1
+    {:alt "Photo of a kitten looking menacing."
+     :src (if (is-image (get-in item ["url"])) (get-in item ["url"]) "/images/placeholder.png")}]
+   [:div.pa2.ph3-ns.pb3-ns
+    [:div.dt.w-100.mt1
+     [:div.dtc [:h4.f4.mv0 (get-in item ["title"])]]]]])
