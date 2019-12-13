adcb0035bd673f37611a7ddd1e56df58b07263b1
Josh Waller <mdx.programs@gmail.com>
2019-12-13 17:22:36 -0500

    Adding random background colors

--- src/app/info_card.cljs
+++ src/app/info_card.cljs
@@ -5,11 +5,14 @@
   (or (clojure.string/ends-with? source "jpg")
       (clojure.string/ends-with? source "png")))
 
+(def bg-colors ["lightblue" "tomato" "lightgreen" "violet" "lightcyan" "skyblue" "#ccc" "#777" "gold"])
+
 (defn info-card [item handle-item-click]
   [:div.w-100.w-50-m.w-25-l.pa3.card
-   {:on-click (fn [e] (.preventDefault e) (handle-item-click item))}
+   {:on-click (fn [e] (.preventDefault e) (handle-item-click item))
+    :style {:background (rand-nth bg-colors)}}
    [:img.db.w-100.br2.br--top.shadow-1
-    {:alt "Photo of a kitten looking menacing."
+    {:alt (get-in item ["title"])
      :src (if (is-image (get-in item ["url"])) (get-in item ["url"]) "/images/placeholder.png")}]
    [:div.pa2.ph3-ns.pb3-ns
     [:div.dt.w-100.mt1
