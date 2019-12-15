e76b6685868d000f1f69433d1f3916c2f63b6802
Josh Waller <mdx.programs@gmail.com>
2019-12-14 21:34:25 -0500

    Removing search filter logic

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -8,7 +8,6 @@
 (def hdurl (r/atom ""))
 (def title (r/atom ""))
 (def show-explanation (r/atom false))
-(def filtered-results (r/atom nil))
 
 (defn handle-item-click [item]
   (reset! show-explanation true)
@@ -22,17 +21,8 @@
   (reset! title "")
   (reset! hdurl ""))
 
-(defn handle-reset-results []
-  (reset! filtered-results @results))
-
 (defn get-results []
-  (GET "apods.json" :handler (fn [r] (reset! results r) (reset! filtered-results r))))
-
-(defn filter-results
-  [e]
-  (let [title (-> e .-target .-value)]
-    (let [new-results (filter (fn [item] (clojure.string/includes? (clojure.string/lower-case (get-in item ["title"])) (clojure.string/lower-case title))) @results)]
-      (reset! filtered-results new-results))))
+  (GET "apods.json" :handler (fn [r] (reset! results r))))
 
 (defn overlay []
   [:div#overlay
@@ -41,20 +31,10 @@
    [:h4.f4.white @title]
    [:div#text @explanation]])
 
-(defn search-filter []
-  [:div#search-filter-wrapper
-   [:input#search-filter {:type "text" :placeholder "Search..." :on-change filter-results}]
-   [:button#reset-search {:on-click handle-reset-results} "Reset"]])
-
-(defn no-results []
-  [:h3#no-results.f3 "No results :("])
-
 (defn cards []
   [:div.article.bg-black
-   [search-filter]
-   (if (= (count @filtered-results) 0) [no-results])
    [:div.cf.flex.flex-wrap
-    (for [item @filtered-results]
+    (for [item @results]
       ^{:key (get-in item ["title"])}
       [info-card item handle-item-click])]])
 
