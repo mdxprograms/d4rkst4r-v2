31e4986927c4874083c91d4aa03cfae48a23eda8
Josh Waller <mdx.programs@gmail.com>
2019-12-14 22:10:52 -0500

    Adding randomizer before writing json file to shuffle results daily

--- fetchapod.js
+++ fetchapod.js
@@ -8,11 +8,17 @@ axios
     "https://api.nasa.gov/planetary/apod?api_key=PEw2feZBRngLi4JWjEDyBfFOcpGyb5clPt0zoZHb"
   )
   .then(res => {
+    if (res.status !== 200) {
+      return console.log("Failed to fetch data. Status: ", 200);
+    }
+
     fs.readFile(JSON_FILE, (err, data) => {
       if (err) return console.error("unable to write to apods.json");
       let json = JSON.parse(data);
 
       json.push(res.data);
-      fs.writeFileSync(JSON_FILE, JSON.stringify(json));
+      fs.writeFileSync(JSON_FILE, JSON.stringify(json.sort(() => Math.random() - 0.5)));
     });
+
+    return console.log("Fetched APOD data successfully");
   });
